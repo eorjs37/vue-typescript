8e0e775c25e13aae91e91d111a7910f279d4d1ba
macdaegeon <chleorjs37@gmail.com>
2023-04-30 10:00:36 +0900

    v-once 제거

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,5 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const message = ref('안녕하세요');
-
-const changeMessagee = () => {
-   message.value = '변경되었습니다.';
-};
-</script>
+<script setup lang="ts"></script>
 
 <template>
-   <main>
-      <span v-once>
-         {{ message }}
-      </span>
-      <span>
-         {{ message }}
-      </span>
-      <button @click="changeMessagee">메세지 변경</button>
-   </main>
+   <main></main>
 </template>
