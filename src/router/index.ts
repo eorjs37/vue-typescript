191fd39757c9f80fddbfab40f859d3ce2d40a488
macdaegeon <chleorjs37@gmail.com>
2023-05-11 11:52:24 +0900

    기본파일 삭제

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,11 +10,6 @@ const router = createRouter({
          component: HomeView
       },
       {
-         path: '/about',
-         name: 'about',
-         component: () => import('../views/AboutView.vue')
-      },
-      {
          path: '/blog-write',
          name: 'blog-write',
          component: () => import('../views/BlogWrite.vue')
