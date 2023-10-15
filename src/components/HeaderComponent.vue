c2f2e21f4b052561651e9f75946f44e11b3f4cc8
macdaegeon <chleorjs37@gmail.com>
2023-10-15 19:42:55 +0900

    fix : 처리

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -8,7 +8,7 @@
             <span class="line"></span>
          </label>
          <nav>
-            <a>로그인111</a>
+            <a>로그인</a>
             <a>회원가입</a>
          </nav>
       </div>
@@ -84,11 +84,13 @@ defineExpose({
 
 <style scoped>
 @import '../assets/_variables.css';
+
 header {
    height: 50px;
    width: 100%;
    background-color: var(--blue);
 }
+
 .head-container {
    display: flex;
    justify-content: space-between;
@@ -105,20 +107,21 @@ header {
    margin-left: 10px;
 }
 
-input[type='checkbox']:checked ~ .hambuger > .line:nth-child(1) {
+input[type='checkbox']:checked~.hambuger>.line:nth-child(1) {
    transform: rotate(45deg);
    top: 15px;
 }
 
-input[type='checkbox']:checked ~ .hambuger > .line:nth-child(2) {
+input[type='checkbox']:checked~.hambuger>.line:nth-child(2) {
    display: none;
 }
-input[type='checkbox']:checked ~ .hambuger > .line:nth-child(3) {
+
+input[type='checkbox']:checked~.hambuger>.line:nth-child(3) {
    transform: rotate(-45deg);
    top: 15px;
 }
 
-.hambuger > .line {
+.hambuger>.line {
    display: inline-block;
    position: absolute;
    width: 100%;
@@ -128,22 +131,21 @@ input[type='checkbox']:checked ~ .hambuger > .line:nth-child(3) {
    transition: 0.25s ease-in-out;
 }
 
-.hambuger > .line:nth-child(1) {
+.hambuger>.line:nth-child(1) {
    top: 0;
 }
 
-.hambuger > .line:nth-child(2) {
+.hambuger>.line:nth-child(2) {
    top: 13px;
 }
 
-.hambuger > .line:nth-child(3) {
+.hambuger>.line:nth-child(3) {
    top: 26px;
 }
 
-nav > a {
+nav>a {
    margin-right: 10px;
    color: var(--white);
    font-weight: bold;
    cursor: pointer;
-}
-</style>
+}</style>
