04cae9fae23063e5a4418d0d4217f68bb3aaa198
macdaegeon <chleorjs37@gmail.com>
2024-04-14 13:08:43 +0900

    fix : socket

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,18 @@
 import { RouterView } from 'vue-router';
 import { onMounted, ref } from 'vue';
 import { getBlogList } from './api/blogApi';
-
+import SockJs from 'sockjs-client';
 interface Blog {
    title: string;
    contents: string;
 }
+
+const sock = () => {
+   const sockJs = new SockJs('https://biggunsite.co.kr/ws/chat');
+   console.log(sockJs);
+};
+
+sock();
 
 onMounted(() => {
    const list = ref<Blog>({ title: '', contents: '' });
