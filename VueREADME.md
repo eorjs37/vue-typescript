6e672c080d76d9e17fd56326cca98c7f478d831e
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-04-16 08:28:04 +0900

    vue 유틸리티 진행중

--- VueREADME.md
+++ VueREADME.md
@@ -10,7 +10,7 @@
 ## Ref
 
 ```
-변경 가능한 객체를 반환하도록 한다
+변경 가능한 객체를 반환하도록 한다. 주로 타입형태로 쓰고싶을때 사용된다.
 ```
 
 ```typescript
@@ -23,8 +23,57 @@ const val = ref<number>(99);
 
 ```
 객체의 반응형 프락시(proxy)를 반환합니다.
+주로 객체형태로 만들때 편하다
 ```
 
 ```typescript
+const obj = reactive<person>({ age: 31, name: '최대건' });
+const obj2 = reactive<person>({ age: 26, name: '김아무개' });
+```
 
+```html
+<template>
+   <h1>{{ obj.age }} / {{ obj.name }}</h1>
+   <h2>{{ obj2.age }} / {{ obj2.name }}</h2>
+</template>
+```
+
+## WatchEffect
+
+```
+ref,reactive로 선언된 것을 감시한다. 이전상태,현재상태를 알 수는 없다.
+```
+
+```typescript
+//Ref
+const val = ref<number>(99);
+const obj2 = reactive<person>({ age: 26, name: '김아무개' });
+
+//watchEffect
+watchEffect(() => console.log(val.value, obj2.age), {
+   flush: 'post'
+});
+```
+
+## Watch
+
+```
+vue options Api와 같이 사용하면 된다.
+다만 여러가지 사용할 경우 배열로 사용이 가능하다.
+```
+
+```typescript
+const state = reactive({
+   count: 0
+});
+
+const counts = ref(0);
+
+watch(
+   () => [state.count, counts.value],
+   ([count, countsvalue], [prevCount, precountsvalue]) => {
+      console.log('state.count ', count, prevCount);
+      console.log('counts.value ', countsvalue, precountsvalue);
+   }
+);
 ```
--- src/views/compostion/CompositionView.vue
+++ src/views/compostion/CompositionView.vue
@@ -3,7 +3,7 @@ interface person {
    age: number;
    name?: string;
 }
-import { computed, reactive, ref, watchEffect } from 'vue';
+import { computed, reactive, readonly, ref, toRef, watch, watchEffect } from 'vue';
 
 //Ref
 const val = ref<number>(99);
@@ -13,14 +13,56 @@ const plusOne = computed(() => val.value + 1);
 
 //Reactive
 const obj = reactive<person>({ age: 31, name: '최대건' });
+const obj2 = reactive<person>({ age: 26, name: '김아무개' });
+//Readonly
+const copy = readonly(obj);
+console.log(copy);
+
+obj.age++;
+//obj2.age++;
 
 //watchEffect
-watchEffect(() => console.log(val.value));
+watchEffect(() => console.log(val.value, obj2.age), {
+   flush: 'post'
+});
 
-val.value++;
+//watch
+const state = reactive({
+   count: 0
+});
+
+const counts = ref(0);
+
+watch(
+   () => [state.count, counts.value],
+   ([count, countsvalue], [prevCount, precountsvalue]) => {
+      console.log('state.count ', count, prevCount);
+      console.log('counts.value ', countsvalue, precountsvalue);
+   }
+);
+
+const addCount = () => {
+   state.count++;
+};
+const addCounts = () => {
+   counts.value++;
+};
+
+const personobject = reactive({
+   foo: 1,
+   bar: 2
+});
+
+const fooRef = toRef(personobject, 'foo');
+fooRef.value++;
+console.log(fooRef.value);
+console.log(personobject.foo);
 </script>
 <template>
    <h1>{{ val }} / {{ plusOne }}</h1>
    <h1>{{ obj.age }} / {{ obj.name }}</h1>
+   <h2>{{ obj2.age }} / {{ obj2.name }}</h2>
+   <button @click="addCount">버튼</button>
+   <button @click="addCounts">버튼</button>
 </template>
 <style scoped></style>
