2985213141e27c2a0efb77e1505471688f670666
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-04-23 08:00:49 +0900

    vuetify 설정

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import './assets/main.css';
 import './assets/fontawesome/fontawesome';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+//vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify/lib/framework.mjs'; 
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+    components,
+    directives
+})
 
 const app = createApp(App);
 
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router);
-
+app.use(vuetify)
 app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,6 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-
-const move = (pagename: string) => {
-   router.push({
-      name: pagename
-   });
-};
 </script>
 
-<template></template>
-
-<style scoped>
-.container {
-   max-width: 1200px;
-   width: 100%;
-   margin: 0 auto;
-}
-
-.page_list {
-   display: flex;
-}
-
-.page_item {
-   margin-left: 5px;
-}
-
-.anchor {
-   padding: 10px;
-   background-color: var(--blue);
-   color: var(--white);
-   line-height: 1;
-   display: flex;
-   align-items: center;
-   box-sizing: border-box;
-   cursor: pointer;
-}
-</style>
+<template>
+    <v-alert title="Alert title" text="..." type="success"></v-alert>
+</template>
