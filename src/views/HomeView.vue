2caf97f0c6a4bd7e3603153813597ddaf0d8b09c
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-04-24 07:30:42 +0900

    fix : infinite scroll 진행중

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,34 @@
+
 <script setup lang="ts">
+import { ref } from 'vue';
+
+
+const items = ref<number[]>(Array.from({length:30},(_,v) => v+1));
+async function api():Promise<number[]> {
+    return new Promise(resolve=>{
+        
+        setTimeout(() => {
+            resolve(Array.from({length:10},(_,v:number) => 1+ (items.value.at(-1) as number) + v ))
+        }, 1000);
+    })
+}
+
+
+async function load({done}:any) {
+    const res:number[] = await api();
+
+    items.value.push(...res)
+    done();
+}
+
 </script>
 
 <template>
-    <v-alert title="Alert title" text="..." type="success"></v-alert>
+    <v-infinite-scroll :height="300" :items="items" :onLoad="load">
+        <template v-for="(item, index) in items" :key="item">
+            <div :class="['pa-2', index % 2 === 0 ? 'bg-grey-lighten-2' : '']">
+            Item number #{{ item }}
+            </div>
+        </template>
+    </v-infinite-scroll>
 </template>
