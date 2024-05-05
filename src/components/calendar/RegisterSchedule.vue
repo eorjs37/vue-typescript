b662ab65d6a7804cc5d148313b9060ef3af28750
macdaegeon <chleorjs37@gmail.com>
2024-05-05 21:21:17 +0900

    fix : 달력 추가 진행중

---
+++ src/components/calendar/RegisterSchedule.vue
@@ -0,0 +1,20 @@
+
+<script setup lang="ts">
+
+defineProps({
+  dialog:{
+    type:Boolean,
+    required:false,
+    default:true
+  }
+})
+
+
+</script>
+<template>
+  <v-dialog max-width="240" >
+    <v-card title="Dialog 2">
+      <template v-slot:text></template>
+    </v-card>
+  </v-dialog>
+</template>
--- src/views/calendar/CalendarView.vue
+++ src/views/calendar/CalendarView.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
 import CalendarComp from "@/components/calendar/CalendarComp.vue";
+import RegisterSchedule from "@/components/calendar/RegisterSchedule.vue"
 </script>
 <template>
   <v-container>
     <CalendarComp/>
+    <RegisterSchedule/>
   </v-container>
 </template>
