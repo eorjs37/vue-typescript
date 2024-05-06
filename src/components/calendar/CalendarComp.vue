320da77398c7836ce46b513e0bdfc1152e34e28e
macdaegeon <chleorjs37@gmail.com>
2024-05-06 20:23:37 +0900

    feat : 달력 진행중

--- src/components/calendar/CalendarComp.vue
+++ src/components/calendar/CalendarComp.vue
@@ -1,8 +1,22 @@
 
 <script lang="ts" setup>
+import { toRef,ref } from "vue";
+
+const props = defineProps({
+  currentdate:{
+    type:Date,
+    required:false,
+    default:new Date()
+  }
+})
+
+const currentDate = toRef(props,"currentdate");
 </script>
 <template>
-  <VCalendar expanded/>
+  <h1>
+    {{ currentDate }}
+  </h1>
+  <VDatePicker expanded v-model="currentDate" mode="date"/>
 </template>
 <style scoped>
 </style>
