d1597ed97254c852c4c5ad71ac0765cc25ba2b09
macdaegeon <chleorjs37@gmail.com>
2024-05-16 21:47:01 +0900

    feat : 시작시간/종료시간 추가

--- src/components/calendar/RegisterSchedule.vue
+++ src/components/calendar/RegisterSchedule.vue
@@ -1,6 +1,8 @@
 
 <script setup lang="ts">
-import { toRef } from "vue";
+import { ref, toRef } from "vue";
+
+
 const props = defineProps({
   dialog:{
     type:Boolean,
@@ -9,21 +11,63 @@ const props = defineProps({
   }
 })
 
+const timeList = ref<string[]>([]);
+const minList = ref<string[]>([]);
+
+for(let  i = 0 ; i < 24 ; i++){
+  timeList.value.push(i > 9 ? String(i) : `0${i}`);
+}
+
+for(let  i = 0 ; i < 60 ; i+=10){
+  minList.value.push(i > 9 ? String(i) : `0${i}`);
+}
+
 defineEmits(["close-dialog"])
 
 const dialog = toRef(props,"dialog");
 </script>
 <template>
-  <v-dialog max-width="500" :model-value="dialog" persistent>
+  <v-dialog max-width="600" :model-value="dialog" persistent>
     <v-card title="예약등록">
       <template v-slot:text>
         <div class="d-flex flex-column">
           <v-select
             clearable
-            label="Select"
+            label="회의실선택"
             variant="outlined"
             :items="[{title:'소회의실1',value:'A0001'},{title:'소회의실2',value:'A0002'},{title:'4층',value:'A0003'}]"
           ></v-select>
+          <h6 class="text-h6">시작시간</h6>
+          <div class="d-flex">
+            <v-select
+              clearable
+              label="시간"
+              variant="outlined"
+              :items="timeList"
+            ></v-select>
+            <v-select
+              clearable
+              label="분"
+              variant="outlined"
+              :items="minList"
+            ></v-select>
+          </div>
+          <h6 class="text-h6">종료시간</h6>
+          <div class="d-flex">
+            <v-select
+              clearable
+              label="시간"
+              variant="outlined"
+              :items="timeList"
+            ></v-select>
+            <v-select
+              clearable
+              label="분"
+              variant="outlined"
+              :items="minList"
+            ></v-select>
+          </div>
+         
         </div>
       </template>
       <template v-slot:actions>
