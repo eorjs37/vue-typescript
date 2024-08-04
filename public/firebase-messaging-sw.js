5b2d8f1326f02c5e4634476f41c7c8b6d0befd9c
macdaegeon <chleorjs37@gmail.com>
2024-08-04 15:21:40 +0900

    feat push 알림 추가

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,39 @@
+// importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js");
+// importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js");
+
+importScripts("https://www.gstatic.com/firebasejs/10.5.2/firebase-app-compat.js")
+importScripts("https://www.gstatic.com/firebasejs/10.5.2/firebase-messaging-compat.js")
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBaXYPHexb32vmjW-Xj-N2oIn4N4TClBz4",
+  authDomain: "webpush-a5e3f.firebaseapp.com",
+  projectId: "webpush-a5e3f",
+  storageBucket: "webpush-a5e3f.appspot.com",
+  messagingSenderId: "180140771495",
+  appId: "1:180140771495:web:465246b4183bd23f6b3382",
+  measurementId: "G-Y3BB9P587V"
+})
+
+const messaging = firebase.messaging();
+
+messaging.onMessage((payload)=>{
+  console.log(payload);
+})
+
+messaging.onBackgroundMessage((payload)=>{
+  console.log(payload);
+  const notification = payload.notification;
+
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    notification
+  );
+
+  const notificationTitle = notification.title;
+  const notificationOptions = {
+    body: notification.body,
+    icon:"/images/icon/logo.png"
+  }
+
+  self.registration.showNotification(notificationTitle,notificationOptions)
+})
--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,46 @@ import { RouterView } from "vue-router";
 import { onMounted, ref } from "vue";
 import { getBlogList } from "./api/blogApi";
 import SockJs from "sockjs-client";
+import * as firebaseApp from "firebase/app";
+import * as firebassMessage from "firebase/messaging"
+
+
+// VAPID_PUBLIC_KEY
+const VAPID_PUBLIC_KEY:string = "BAMSegYiIZvlHZD_YLRsK9ILwwRhDKA893tj7KDg90kAiPjrulcJD2hY3WLkNfgGVLGUzmUC8IpjYMURCZ8MC70"
+
+// firebaseApp init
+firebaseApp.initializeApp(
+  {
+    apiKey: "AIzaSyBaXYPHexb32vmjW-Xj-N2oIn4N4TClBz4",
+    authDomain: "webpush-a5e3f.firebaseapp.com",
+    projectId: "webpush-a5e3f",
+    storageBucket: "webpush-a5e3f.appspot.com",
+    messagingSenderId: "180140771495",
+    appId: "1:180140771495:web:465246b4183bd23f6b3382",
+    measurementId: "G-Y3BB9P587V"
+  }
+)
+
+const messaging = firebassMessage.getMessaging();
+firebassMessage.getToken(messaging,{
+  vapidKey:VAPID_PUBLIC_KEY
+})
+  .then(currentToken=>{
+    if(currentToken){
+      console.log(currentToken);
+    }
+  })
+  .catch((err)=>{
+    console.error("getToken error : ",err);
+  
+  })
+
+firebassMessage.onMessage(messaging,(payload)=>{
+  console.log("paylaod : ",payload);
+})
+
+
+
 interface Blog {
   title: string;
   contents: string;
