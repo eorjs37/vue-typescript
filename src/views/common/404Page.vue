ffa5b864ed2d27659cade0c9d641f4116752622d
macdaegeon <chleorjs37@gmail.com>
2024-10-03 16:37:55 +0900

    fix : 404page not found change text

--- src/views/common/404Page.vue
+++ src/views/common/404Page.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <h1 class="text-center">404</h1>
-    <h2 class="text-center">Page Not Found</h2>
+    <h2 class="text-center">Page Not Found-</h2>
   </v-container>
   
 </template>
